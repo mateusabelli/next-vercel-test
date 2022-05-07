8b849ada42be78b6d696b0ea353b8e447bfe0332
Mateus Abelli <43862225+MateusAbelli@users.noreply.github.com>
2022-05-07 10:59:11 -0300

    Added About page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import Link from "next/link";
+import About from "./about";
 
 function Home() {
   const [counter, setCounter] = useState(1);
@@ -7,6 +9,9 @@ function Home() {
     <>
       <h2>{counter}</h2>
       <button onClick={() => setCounter(counter + 1)}>Add</button>
+      <Link href="./about">
+        <button>About</button>
+      </Link>
     </>
   );
 }
